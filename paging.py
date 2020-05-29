846d38eeb98d3273c57a75e02b093ba98aac8f7d
georgeo <georgeosabu@gmail.com>
2020-05-29 14:28:29 +0200

    fifo algorithm

--- paging.py
+++ paging.py
@@ -7,6 +7,31 @@
 import sys
 import random
 
+def FIFO(size,pages):
+    pFaults=0
+    stack=[]
+    for page in pages:
+        if(len(stack)!=size):
+            stack.append(page)
+            pFaults=pFaults+1
+        else:
+            if(page in stack):
+                continue
+            else:
+                for i in range(size):
+
+                    if(i==size-1):
+                        stack.pop()
+                        stack.append(page)
+                        
+                    else:
+                        stack[i]=stack[i+1]
+                
+                #stack.append(page)
+                pFaults=pFaults+1
+       
+
+    return pFaults
 
 def main():
     size = int(sys.argv[1])
@@ -14,9 +39,10 @@ def main():
         print("Your frame size should be an integer between 1 and 7")
         exit()
     pages=""
-    for x in range(9):
-        pages=pages+str(random.randint(0,9))
-    #print "FIFO", FIFO(size,pages), "page faults."
+    #for x in range(9):
+    #    pages=pages+str(random.randint(0,9))
+    pages="2748064263683623842273793235"
+    print ("FIFO", FIFO(size,pages), "page faults.")
     #print "LRU", LRU(size,pages), "page faults."
     #print "OPT", OPT(size,pages), "page faults."
 
